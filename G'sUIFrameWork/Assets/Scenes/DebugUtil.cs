957a5f73c1fe9040de42949546893a0322a5d78b
Wenguang <wenguang@playbiba.com>
2015-03-25 15:11:07 -0700

    表面bug解决
    把很多东西换成用screen控制的，而不是scene

---
+++ G'sUIFrameWork/Assets/Scenes/DebugUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class DebugUtil
+{
+	public static void Assert(bool condition,string message)
+	{
+		if(!condition)
+		{
+			UnityEngine.Debug.Log(message);
+		}
+	}
+
+}
